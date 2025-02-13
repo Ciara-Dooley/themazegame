80da8507e4a3a995221a65843b956354cce1619f
Ciara-Dooley <ciara.dooley28@student.tamdistrict.org>
2025-02-13 21:02:51 +0000

    gand

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,69 +1,71 @@
 import { Scene } from 'phaser';
 
-export class Game extends Scene
-{
+export class Game extends Scene {
+    cusors: Phaser.Types.Input.Keyboard.CursorKeys;
+
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
-    msg_text : Phaser.GameObjects.Text;
+    msg_text: Phaser.GameObjects.Text;
 
-    constructor ()
-    {
+    constructor() {
         super('Game');
     }
-     preload()
-      {
-        
-            this.load.image('sky', 'assets/sky.png');
-            this.load.image('ground', 'assets/platform.png');
-            this.load.image('star', 'assets/star.png');
-            this.load.image('bomb', 'assets/bomb.png');
-            this.load.spritesheet('dude', 
-                'assets/dude.png',
-                { frameWidth: 32, frameHeight: 48 }
-            );
-     }
-     platforms: Phaser.Physics.Arcade.StaticGroup;
-     player: any;
-    create ()
-    {
+    preload() {
+
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude',
+            'assets/dude.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
+    }
+    platforms: Phaser.Physics.Arcade.StaticGroup;
+    player: any;
+    create() {
+        this.cusors = this.input.keyboard!.createCursorKeys();
         this.add.image(400, 300, 'sky');
 
         this.platforms = this.physics.add.staticGroup();
 
-        this.platforms.create(400,568,'ground').setScale(2).refreshBody();
-        this.platforms.create(600,400,'ground');
-        this.platforms.create(50,250,'ground');
-        this.platforms.create(750,220,'ground');
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
 
         this.player = this.physics.add.sprite(100, 450, 'dude');
 
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
 
-this.anims.create({
-    key: 'left',
-    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-    frameRate: 10,
-    repeat: -1
-});
+        this.physics.collide(this.player, this.platforms);
 
-this.anims.create({
-    key: 'turn',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 20
-});
 
-this.anims.create({
-    key: 'right',
-    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-    frameRate: 10,
-    repeat: -1
-});
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'turn',
+            frames: [{ key: 'dude', frame: 4 }],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
 
     }
 
-    update()
-     {
-            
-        }
+    update() {
+
+    }
 }
